8790c8062cc575906e7baff02d2f20f1300a5a60
Parthibg00410769 <g00410769@atu.ie>
2022-11-30 00:32:49 +0000

    first to inheritance person1

---
+++ src/main/java/ie/atu/week10/Person.java
@@ -0,0 +1,12 @@
+package ie.atu.week10;
+
+
+
+public class Person {
+    public static void main(String[] args) {
+        Person dave = new Person();
+        Customer myCust = new Customer("parthib", "45354","salthill", "34554354534",true);
+        System.out.println("Details"+ myCust.toString());
+
+    }
+}
